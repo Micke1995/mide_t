b7f53ca3ffcf77d75849f97166892a212dee0b82
Micke1995 <mickejuarez15@gmail.com>
2026-03-20 15:00:27 -0600

    hotfix sistema de medicion

--- libreria/views.py
+++ libreria/views.py
@@ -5,12 +5,12 @@ from django.http import JsonResponse
 
 
 def index(request):
-    
+
     sis_name = "Zona Morelia Centro"
     try:
         sistema = SistemaMedicion.objects.get(nombre=sis_name)
     except SistemaMedicion.DoesNotExist:
         return JsonResponse({"error": f"Sistema '{sis_name}' no encontrado"}, status=404)
-    
+
     context = {'sistema':sistema}
     return render(request, f'views/inicio.html',context)
